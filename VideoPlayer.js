8346cc8db2cbe42c853ed2acfe33aaa9a4b7bfe4
Alan Duanmu <alan.duanmu@gmail.com>
2018-05-20 12:51:31 -0700

    Update VideoPlayer.js

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -1062,6 +1062,7 @@ export default class VideoPlayer extends Component {
      * Show loading icon
      */
     renderLoader() {
+        return null;
         if ( this.state.loading ) {
             return (
                 <View style={ styles.loader.container }>
